c347eff31033a3df678656574a6fc39477f64e90
niveshsengar <niveshsengar@gmail.com>
2023-06-24 14:47:07 +0530

    design-1 Initial Commit

---
+++ src/test/java/com/pbma/oneview/OneViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbma.oneview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

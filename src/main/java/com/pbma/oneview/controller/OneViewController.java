838b52abd17cbfc3a872b949a26454b7188ffc96
Suprabha-12 <suprabhakundu12@gmail.com>
2023-06-28 16:21:49 +0530

    Update OneViewController.java

--- src/main/java/com/pbma/oneview/controller/OneViewController.java
+++ src/main/java/com/pbma/oneview/controller/OneViewController.java
@@ -3,8 +3,10 @@ package com.pbma.oneview.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

ca595d64ee26b7304e3f04ba9a853c21df6fe50a
Nishi Patodi <nishi.patodi@gmail.com>
2023-07-02 18:42:53 +0530

    minor fixes

--- src/main/java/com/pbma/oneview/entity/Trainee.java
+++ src/main/java/com/pbma/oneview/entity/Trainee.java
@@ -1,20 +1,12 @@
 package com.pbma.oneview.entity;
 
 import java.time.LocalDate;
-
-import org.antlr.v4.runtime.misc.NotNull;
-
 import jakarta.persistence.*;
 
 import lombok.Builder;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Negative;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
@@ -31,6 +23,7 @@ public class Trainee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 	
 	@Column(name = "name")
@@ -78,12 +71,157 @@ public class Trainee {
 	@Pattern(regexp = "\\d{6}", message = "Invalid Pincode")
 	private Integer pin;
 
-	@OneToOne
-	@JoinColumn(referencedColumnName = "trainingId")
+	@OneToOne(cascade = {CascadeType.ALL})
+	@JoinColumn(name = "training", referencedColumnName = "training_id")
 	private Training training;
 
-	@OneToOne
-	@JoinColumn(referencedColumnName = "familyId")
+	@Override
+	public String toString() {
+		return "Trainee [id=" + id + ", name=" + name + ", gender=" + gender + ", dateOfBirth=" + dateOfBirth
+				+ ", idType=" + idType + ", idNumber=" + idNumber + ", married=" + married + ", email=" + email
+				+ ", address=" + address + ", district=" + district + ", state=" + state + ", pin=" + pin
+				+ ", training=" + training + ", familyInfo=" + familyInfo + ", traineeDetails=" + traineeDetails
+				+ ", assessment=" + assessment + ", placement=" + placement + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getIdType() {
+		return idType;
+	}
+
+	public void setIdType(String idType) {
+		this.idType = idType;
+	}
+
+	public Integer getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(Integer idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	public Boolean getMarried() {
+		return married;
+	}
+
+	public void setMarried(Boolean married) {
+		this.married = married;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Integer getPin() {
+		return pin;
+	}
+
+	public void setPin(Integer pin) {
+		this.pin = pin;
+	}
+
+	public Training getTraining() {
+		return training;
+	}
+
+	public void setTraining(Training training) {
+		this.training = training;
+	}
+
+	public FamilyBackground getFamilyInfo() {
+		return familyInfo;
+	}
+
+	public void setFamilyInfo(FamilyBackground familyInfo) {
+		this.familyInfo = familyInfo;
+	}
+
+	public TraineeDetails getTraineeDetails() {
+		return traineeDetails;
+	}
+
+	public void setTraineeDetails(TraineeDetails traineeDetails) {
+		this.traineeDetails = traineeDetails;
+	}
+
+	public AssessmentDetails getAssessment() {
+		return assessment;
+	}
+
+	public void setAssessment(AssessmentDetails assessment) {
+		this.assessment = assessment;
+	}
+
+	public PlacementDetails getPlacement() {
+		return placement;
+	}
+
+	public void setPlacement(PlacementDetails placement) {
+		this.placement = placement;
+	}
+
+	@OneToOne(cascade = {CascadeType.ALL})
+	@JoinColumn(name = "family_info", referencedColumnName = "family_id")
 	private FamilyBackground familyInfo;
 
 	@OneToOne(cascade = {CascadeType.ALL})
--- src/main/java/com/pbma/oneview/entity/Training.java
+++ src/main/java/com/pbma/oneview/entity/Training.java
@@ -14,16 +14,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Training {
-
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "training_id")
 	public Integer trainingId;
+
+	@Column(name = "training_name")
 	private String trainingName;
+
+	@Column(name = "training_address")
 	private String trainingAddress;
+
+	@Column(name = "training_location")
 	private String trainingLocation;
+
 	@OneToOne(cascade = {CascadeType.ALL})
 	@JoinColumn(name = "batch", referencedColumnName = "batch_id")
 	private Batch batch;
+
+
 	
 }

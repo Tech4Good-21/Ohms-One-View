6e3cd3319705a5dde39c8eddbedd844f7df8409e
Nishi Patodi <nishi.patodi@gmail.com>
2023-07-02 18:32:41 +0530

    added validation

--- src/main/java/com/pbma/oneview/entity/Training.java
+++ src/main/java/com/pbma/oneview/entity/Training.java
@@ -15,22 +15,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Training {
 
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "training_id")
 	public Integer trainingId;
-
-	@Column(name = "training_name")
 	private String trainingName;
-
-	@Column(name = "training_address")
 	private String trainingAddress;
-
-	@Column(name = "training_location")
 	private String trainingLocation;
-	
 	@OneToOne(cascade = {CascadeType.ALL})
-	@JoinColumn(name = "batch", referencedColumnName = "id")
+	@JoinColumn(name = "batch", referencedColumnName = "batch_id")
 	private Batch batch;
 	
 }
